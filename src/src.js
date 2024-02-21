7beab218811f47a46aa96810d93b4b1886c87cbc
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-02-21 12:40:28 -0500

    21-feb class

--- src/src.js
+++ src/src.js
@@ -1,3 +1,33 @@
+//-------VARIABLES-------*/
+let nam = "Pancracio de la Santa Circuncinción";
+let confirmation = undefined;
+
+/*------Operador ternario------*/
+confirmation === "Pancracio" ? "Es el valor" : "No es el valor";
+console.log(confirmation);
+
+/*-----------Constantes-----------*/
+const tonto = true;
+
+
+/*---------Metodos propios del lenguaje----------*/
+let pi2 = undefined;
+pi2 = Math.PI;
+console.log(nam.includes("Pancracio"));
+console.log(nam.length);
+
+//----------Method & functions---------*/
+//Arrow function
+const sum = (a, b) => {
+    return a+b;
+}
+
+// Cuando es un solo parametro
+const saludar = userName =>{
+    
+}
+
+
 const dayScene = Atropos({
     el: '.a-day',
     activeOffset: 100,
